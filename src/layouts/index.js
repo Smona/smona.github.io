9ce056fd5257979251233471ee8fc0e6f7b8d601
Mason Bourgeois <mason.bourgeois@gmail.com>
2018-05-28 05:19:43 -0500

    begin designing landing page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,13 +13,13 @@ const Layout = ({ children, data }) => (
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
-    />
-    <Header siteTitle={data.site.siteMetadata.title} />
+    >
+      <link href="https://fonts.googleapis.com/css?family=Titan+One|Open+Sans" rel="stylesheet" />
+    </Helmet>
+    {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
         paddingTop: 0,
       }}
     >
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import SpaceSVG from './space.svg';
+
+import './index.css';
 
 const IndexPage = () => (
   <div>
+    <div id="hero">
+      <img src={SpaceSVG} />
+      <div className="hero__content">
+        <h1>Mason Bourgeois</h1>
+        <p className="hero__subtitle">
+          Web Developer Extraordinaire
+        </p>
+      </div>
+    </div>
     <h1>Projects</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
